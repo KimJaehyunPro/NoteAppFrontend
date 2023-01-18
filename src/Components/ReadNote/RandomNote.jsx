a6b0b2e1a2bfae8c3baa5176af8b7ac98f624dba
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-01-18 18:30:34 +0900

    RandomNote finished

--- src/Components/ReadNote/RandomNote.jsx
+++ src/Components/ReadNote/RandomNote.jsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
-import useRandomNote from "../../Hooks/useRandomNote";
 import { GET_RANDOM_NOTE_ID_API_RUL } from '../../Constants/endpoints';
 
 export default function RandomNote(props) {
     
     const navigate = useNavigate();
-    const [noteId, setNoteId] = React.useState('');
 
     React.useEffect(() => {
         fetch(`${process.env.REACT_APP_BACKEND_URL}/${GET_RANDOM_NOTE_ID_API_RUL}`)
             .then(response => response.json())
             .then(data => {
-                setNoteId(data.noteId);
                 return navigate(`../../notes/${data.noteId}`);
             })
-    }, [noteId])
+    })
 }
