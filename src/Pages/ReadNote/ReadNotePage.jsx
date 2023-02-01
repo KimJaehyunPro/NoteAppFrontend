523d934ebcfa800658bc8401d4c321b95ec7e4a5
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-02-01 16:53:37 +0900

    NOTE-13: Line break to ReadNotePage implemented

--- src/Pages/ReadNote/ReadNotePage.jsx
+++ src/Pages/ReadNote/ReadNotePage.jsx
@@ -1,4 +1,4 @@
-import { Paper, Fab } from "@mui/material";
+import { Paper, Fab, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import * as React from 'react';
 import useNote from "../../Hooks/useNote";
@@ -11,9 +11,11 @@ export default function ReadNotePage(props) {
 
     return (
         <Paper elevation={3}>
-            <p>{noteId}</p>
-            <p>{title}</p>
-            <p>{content}</p>
+            <Typography>{noteId}</Typography>
+            <br/>
+            <Typography>{title}</Typography>
+            <br/>
+            <Typography style={{whiteSpace: 'pre-line'}}>{content}</Typography>
             <Fab color="secondary" aria-label="update" href={`/note/update/${noteId}`}>
                 <EditIcon />
             </Fab>
--- src/Utils/parseTags.js
+++ src/Utils/parseTags.js
@@ -1,4 +1,8 @@
 // '#tag1 #tag2' => ['tag1', 'tag2']
 export default function parseTags(tags) {
+    
+    const tagsAreEmpty = (tags.length === 0)
+    if (tagsAreEmpty) return [];
+
     return tags.split(' ').filter(tag => tag[0] === '#').map(tag => tag.slice(1));
 }
