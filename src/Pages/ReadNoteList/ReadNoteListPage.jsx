da90e864a90ee8f74706f02f04abd5adb2bf8b7d
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-02-15 11:27:13 +0900

    NOTE-56: ReadNoteListPage changed. Render <EmptyNote/> if there is no Note.

--- src/Pages/ReadNoteList/ReadNoteListPage.jsx
+++ src/Pages/ReadNoteList/ReadNoteListPage.jsx
@@ -2,13 +2,17 @@ import Notes from './Notes';
 import useNoteList from './utils/useNoteList';
 import * as React from 'react';
 
+import { Typography } from '@mui/material';
+
+const EmptyNotes = () => {
+    return <Typography>No notes found.</Typography>
+}
+
 export default function ReadNoteListPage(props) {
 
     const [noteList, setNoteList] = useNoteList();
 
     return (
-        <div>
-            {noteList.length > 0 ? <Notes noteList={noteList} setNoteList={setNoteList} /> : <p>There is no note.</p>}
-        </div>
+        noteList.length > 0 ? <Notes noteList={noteList} setNoteList={setNoteList} /> : <EmptyNotes />
     )
 }
