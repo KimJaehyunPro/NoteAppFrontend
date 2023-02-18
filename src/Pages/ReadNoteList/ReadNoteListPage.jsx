1757dc7705a78eae5915707d518fbe58052f8df6
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-02-19 05:07:45 +0900

    NOTE-24: Initiated implementation of pagination

--- src/Pages/ReadNoteList/ReadNoteListPage.jsx
+++ src/Pages/ReadNoteList/ReadNoteListPage.jsx
@@ -1,8 +1,8 @@
 import Notes from './Notes';
 import useNoteList from './utils/useNoteList';
-import * as React from 'react';
 
 import { Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 
 const EmptyNotes = () => {
     return <Typography>No notes found.</Typography>
@@ -11,8 +11,34 @@ const EmptyNotes = () => {
 export default function ReadNoteListPage(props) {
 
     const [noteList, setNoteList] = useNoteList();
+    const [page, setPage] = useState(1);
+
+    const fetchMoreData = () => {
+        setNoteList(prevNotes => [
+            ...prevNotes,
+            { id: 1, title: "hey", content: "Ahoy!", tags: [{id: 53, name: "foo"}] }
+        ]);
+        setPage(prevPage => prevPage + 1);
+    };
+
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            fetchMoreData();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll); 
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     return (
         noteList.length > 0 ? <Notes noteList={noteList} setNoteList={setNoteList} /> : <EmptyNotes />
-    )
+    );
+
+    // const [noteList, setNoteList] = useNoteList();
+
+    // return (
+    //     noteList.length > 0 ? <Notes noteList={noteList} setNoteList={setNoteList} /> : <EmptyNotes />
+    // )
 }
