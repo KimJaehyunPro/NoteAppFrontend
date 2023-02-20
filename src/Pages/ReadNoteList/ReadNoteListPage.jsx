394c64681d7e69f828cb3841bcc4214a9b9e52bb
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-02-20 20:10:44 +0900

    NOTE-6: pagination implemented without any bug

--- src/Pages/ReadNoteList/ReadNoteListPage.jsx
+++ src/Pages/ReadNoteList/ReadNoteListPage.jsx
@@ -1,6 +1,8 @@
 import Notes from './Notes';
 import useNoteList from './utils/useNoteList';
 
+import { NOTE_API_URL } from '../../Constants/endpoints';
+
 import { Typography } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
@@ -14,11 +16,29 @@ export default function ReadNoteListPage(props) {
     const [page, setPage] = useState(1);
 
     const fetchMoreData = () => {
-        setNoteList(prevNotes => [
-            ...prevNotes,
-            { id: 1, title: "hey", content: "Ahoy!", tags: [{id: 53, name: "foo"}] }
-        ]);
-        setPage(prevPage => prevPage + 1);
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/${NOTE_API_URL}/?page=${page}&size=5&sort=id,desc`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(page);
+                const notes = data.content;
+                console.log(notes);
+                console.log(notes[0]);
+
+                setNoteList(prevNotes => {
+                    const finalNotes = prevNotes;
+                    notes.forEach((note) => {
+                        finalNotes.push(note);
+                    })
+                    return finalNotes;
+                })
+
+                // If there is no note, DO NOT listen to 'scroll' event anymore, since there's no note to get anyway.
+                if (notes.length === 0) {
+                    window.removeEventListener('scroll', handleScroll);
+                } else {
+                    setPage(prevPage => prevPage + 1);
+                }
+            })
     };
 
     const handleScroll = () => {
@@ -28,9 +48,9 @@ export default function ReadNoteListPage(props) {
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll); 
+        window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+        }, [page]);
 
     return (
         noteList.length > 0 ? <Notes noteList={noteList} setNoteList={setNoteList} /> : <EmptyNotes />
