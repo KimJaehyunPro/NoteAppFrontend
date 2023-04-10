09a4739568cdb7d6f4313cb78f72c4713f90d429
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-04-11 03:19:04 +0900

    NOTE-86: implemented login page, implemented localStorage

--- src/Pages/utils/getJWSToken.js
+++ src/Pages/utils/getJWSToken.js
@@ -1,24 +1,18 @@
 import { LOGIN_API_URL } from '../../Constants/endpoints';
 import axios from 'axios';
 
-export default function getJWSToken(
-    username,
-    password
-) {
-    username = "testUser1";
-    password = "123";
-    
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/${LOGIN_API_URL}`, {
+export default function getJWSToken(username, password) {
+    return axios
+        .post(`${process.env.REACT_APP_BACKEND_URL}/${LOGIN_API_URL}`, {
             username,
-            password
+            password,
         })
-        .then(response => {
-            
+        .then((response) => {
             if (response.data.accessToken) {
-                localStorage.setItem("token", JSON.stringify(response.data));
-                return localStorage.getItem('token');
-            }
+                localStorage.setItem("accessToken", response.data.accessToken);
+                localStorage.setItem("tokenType", response.data.tokenType);
 
-            return response.data;
+                return { accessToken: localStorage.getItem("accessToken"), tokenType: localStorage.getItem("tokenType") };
+            }
         });
 }
