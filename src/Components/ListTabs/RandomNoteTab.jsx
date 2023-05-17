cb59b00bc3d135766bd408d63b4e759e29a1d0aa
KimJaehyunPro <kim.jaehyun.pro@gmail.com>
2023-05-18 05:22:55 +0900

    NOTE-110: When not logged in, RandomNoteTab is not shown in the first place

--- src/Components/ListTabs/RandomNoteTab.jsx
+++ src/Components/ListTabs/RandomNoteTab.jsx
@@ -11,32 +11,34 @@ import { LOGIN_URL, NOTE_URL } from '../../Constants/endpoints';
 
 export default function RandomNoteTab(props) {
 
-    const randomId = useRandomId();
-
     const navigate = useNavigate();
 
-    const isLoggedIn = (sessionStorage.getItem("tokenType"));
-    React.useEffect(() => {
-        if (!isLoggedIn) {
-            navigate(`../${LOGIN_URL}`);
-        }
-    }, [isLoggedIn])
-    
-    return (
-        <List>
-            <ListItem key="Random" >
-                <ListItemButton onClick={() => {
-                    randomId().then(randomId => {
-                        navigate(`${NOTE_URL}/${randomId}`);
-                    })
-                }}>
-                    <ListItemIcon>
-                        <AnalyticsIcon />
-                    </ListItemIcon>
+    const randomId = useRandomId();
 
-                    <ListItemText primary="Random Page" />
-                </ListItemButton>
-            </ListItem>
-        </List>
+    const isLoggedIn = (sessionStorage.getItem("accessToken") != null);
+
+    return (
+        isLoggedIn ? 
+            <List>
+                <ListItem key="Random" >
+                    <ListItemButton onClick={() => {
+                        randomId().then(randomId => {
+                            if (isLoggedIn) {
+                                navigate(`${NOTE_URL}/${randomId}`);
+                            } else {
+                                navigate(`../${LOGIN_URL}`);
+                            }
+                        })
+                    }}>
+                        <ListItemIcon>
+                            <AnalyticsIcon />
+                        </ListItemIcon>
+
+                        <ListItemText primary="Random Page" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            
+            : null
     )
 }
--- src/Hooks/useRandomId.js
+++ src/Hooks/useRandomId.js
@@ -3,17 +3,17 @@ import { GET_RANDOM_ID_API_URL } from "../Constants/endpoints";
 export default function useRandomId() {
 
     const tokenType = sessionStorage.getItem("tokenType");
-    const token = sessionStorage.getItem("accessToken");
+    const accessToken = sessionStorage.getItem("accessToken");
 
     return () => {
         return fetch(`${process.env.REACT_APP_BACKEND_URL}/${GET_RANDOM_ID_API_URL}`, {
             headers: {
-                'Authorization': `${tokenType}${token}`
+                'Authorization': `${tokenType}${accessToken}`
             }
         })
-            .then(response => response.json())
-            .then((randomId) => {
-                return randomId;
-            })
+        .then(response => response.json())
+        .then((randomId) => {
+            return randomId;
+        })
     }
 }
